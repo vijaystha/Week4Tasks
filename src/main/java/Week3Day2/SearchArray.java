8cce2e4738d70e7af2794db58bc0a6bbbb7586ab
vijaystha <yajibs.bs@gmail.com>
2023-08-15 16:42:44 -0400

    Added changes

---
+++ src/main/java/Week3Day2/SearchArray.java
@@ -0,0 +1,11 @@
+package Week3Day2;
+
+//        write a program to search a particular element in an array : {1,9,4,8,5,7}   find index of 4
+public class SearchArray {
+    public static void main(String[] args) {
+//        int[]a= new int[6];
+        int[] array= {1,9,4,8,5,7};
+
+        System.out.println(array[3]);
+    }
+}
